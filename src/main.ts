5f1e4b391309423d7a96f5ad6803a0dd554ab399
sallarbhutto11 <sallar.bhutto@swipehouse.org>
2024-08-04 15:50:42 +0500

    Added User Res and Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Easygenerator Auth App')
+    .setDescription('Hiring task.')
+    .setVersion('1.0')
+    .addTag('User Auth')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3001);
+  console.log('Backend is up on 3000');
 }
 bootstrap();
